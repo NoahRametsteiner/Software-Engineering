79a4c40382545f20f70366a9a325c3f51458ee3e
NoahRametsteiner <77678379+NoahRametsteiner@users.noreply.github.com>
2022-01-25 11:25:40 +0100

    Function Finished

--- PasswordGenerator/MainWindow.xaml.cs
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.RightsManagement;
@@ -21,20 +22,34 @@ namespace PasswordGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rand = new Random();
+        public String pw = "";
+        public int grossb = 0, kleinb = 0, sonderz = 0, ziffern = 0, size = 0;
+        public String str = "abcdefghijklmnopqrstuvwxyzöäü" + "ABCDEFGHIJKLMNOPQRSTUFWXYZÖÄÜ" + "1234567890" + "!§$%&/()=?*+#;,:.";
+
+        #region MainWindow
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        Random rand = new Random();
 
-        public String pw = "";
+        #endregion
+
+        #region Gen
 
         String Gen()
         {
-            String Password="";
-            int check=0;
-            String str = "abcdefghijklmnopqrstuvwxyzöäü"+"ABCDEFGHIJKLMNOPQRSTUFWXYZÖÄÜ"+"1234567890"+"!§$%&/()=?*+#;,:.";
-            int size = 20; ///Länge des Passworts
+            int count = 0;
+            Start:
+
+            kleinb = Convert.ToInt32(text4.Text);
+            grossb = Convert.ToInt32(text3.Text);
+            ziffern = Convert.ToInt32(text1.Text);
+            sonderz = Convert.ToInt32(text2.Text);
+            String Password = "";
+            int check = 0;
+            size = grossb+ kleinb+ sonderz+ ziffern; ///Länge des Passworts
 
             for (int i = 0; i < size; i++)
             {
@@ -44,18 +59,58 @@ namespace PasswordGenerator
 
             check = checkreq(Password);
             
-            return Password;
+            if (check == 0)
+            {
+                count++;
+                goto Start;
+            }
+            else
+            {
+                Console.WriteLine(count);
+                return Password;
+            }
         }
 
-        checkreq(String pwcheck)
+        #endregion
+
+        #region checkreq
+
+        int checkreq(String pwcheck)
         {
+            int ckecksum = 0, v=0, x=0, y=0, z=0;
 
+            for (int i = 0; i < size; i++)
+            {
+                for (int a = 0; a < 29; a++)
+                {
+                    if (pwcheck[i] == str[a]) { v++; break; }
+                }
+                for (int a = 29; a < 58; a++)
+                {
+                    if (pwcheck[i] == str[a]){ x++; break; }
+                }
+                for (int a = 58; a < 68; a++)
+                {
+                    if (pwcheck[i] == str[a]) { y++; break; }
+                }
+                for (int a = 68; a < 85; a++)
+                {
+                    if (pwcheck[i] == str[a]) { z++; break; }
+                }
+            }
+
+            if (kleinb == v && grossb == x && ziffern == y && sonderz == z) {ckecksum = 1;}
+            else {ckecksum = 0;}
+
+            return ckecksum;
         }
+
+        #endregion
 
         private void Generate_Click(object sender, RoutedEventArgs e)
         {
             pw = Gen();
-            Console.WriteLine(pw);
+            text5.Text = pw;
         }
     }
 }
